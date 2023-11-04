f400441a53ac030815e2b0f4cb158e1ec3a35bc5
Yuliia <yuliia.pochtarova@hneu.net>
2023-11-04 19:11:00 +0200

    Create entity "Folder"

---
+++ FolderSystem/DataAccess/Entities/Folder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entities
+{
+    public class Folder
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public int? ParentFolderId { get; set; }
+
+        public Folder? ParentFolder { get; set; }
+
+        public ICollection<Folder> Subfolders { get; set; }
+    }
+}
