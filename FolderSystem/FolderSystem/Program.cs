11c5c625f850ef5a0a1f465806a89b5ad1ac44f0
Yuliia <yuliia.pochtarova@hneu.net>
2023-11-04 19:21:11 +0200

    Change logic of getting connection string

--- FolderSystem/FolderSystem/Program.cs
+++ FolderSystem/FolderSystem/Program.cs
@@ -1,6 +1,29 @@
+using BusinessLogic.Repositories;
+using DataAccess.EF;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
 var builder = WebApplication.CreateBuilder(args);
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<FoldersContext>(options => options.UseSqlServer(connection));
+builder.Services.AddScoped<FolderRepository>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+       name: "folderRoute",
+       pattern: "{*folderPath}",
+       defaults: new { controller = "Folder", action = "Index" });
 
+    endpoints.MapControllerRoute(
+       name: "default",
+       pattern: "{controller=Home}/{action=Index}/{id?}");
+});
 app.Run();
