8f78759909ed25f3b133da0977c01b6753e4e589
Yuliia <yuliia.pochtarova@hneu.net>
2023-11-04 20:41:04 +0200

    Delete unnecessary namespaces

--- FolderSystem/BusinessLogic/Repositories/FolderRepository.cs
+++ FolderSystem/BusinessLogic/Repositories/FolderRepository.cs
@@ -1,10 +1,7 @@
 ﻿using DataAccess.EF;
 using DataAccess.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+
 
 namespace BusinessLogic.Repositories
 {
